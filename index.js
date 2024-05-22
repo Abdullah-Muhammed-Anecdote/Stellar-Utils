1ffd775ab2e6f4727681e039b7b17cdfc0cc074e
Abdullah <a.mohamed.anecdote@gmail.com>
2024-05-22 15:43:35 +0300

    implemented send mail by sendgrid api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {updateBrevoContact} = require('./updateBrevoContact');
 const {getSmoobuNewAccessToken,getSmoobuApartments, getSmoobuBookingsByApartmentId} = require('./smoobu_utils');
+const {sendMailBySendGrid} = require('./sendgrid_utils');
 
-module.exports = { updateBrevoContact, getSmoobuNewAccessToken,getSmoobuApartments, getSmoobuBookingsByApartmentId };
+module.exports = { updateBrevoContact, getSmoobuNewAccessToken,getSmoobuApartments, getSmoobuBookingsByApartmentId,sendMailBySendGrid };
