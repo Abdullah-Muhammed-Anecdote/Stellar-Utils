0ee41f032a9592b19d74d6b556d1d8c599df6674
Abdullah <a.mohamed.anecdote@gmail.com>
2024-06-12 10:43:28 +0300

    implemented send dynamic template by sendgrid

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {updateBrevoContact} = require('./updateBrevoContact');
 const {getSmoobuNewAccessToken,getSmoobuApartments, getSmoobuBookingsByApartmentId} = require('./smoobu_utils');
-const {sendMailBySendGrid} = require('./sendgrid_utils');
+const {sendMailBySendGrid,sendDynamicTemplateMailBySendGrid} = require('./sendgrid_utils');
 const {sendEmail,getFormUrl,checkAndSendFormLinks} = require('./app_utils');
 
-module.exports = { updateBrevoContact, getSmoobuNewAccessToken,getSmoobuApartments, getSmoobuBookingsByApartmentId,sendMailBySendGrid, sendEmail,getFormUrl,checkAndSendFormLinks };
+module.exports = { updateBrevoContact, getSmoobuNewAccessToken,getSmoobuApartments, getSmoobuBookingsByApartmentId,sendMailBySendGrid, sendEmail,sendDynamicTemplateMailBySendGrid,getFormUrl,checkAndSendFormLinks };
--- sendgrid_utils.js
+++ sendgrid_utils.js
@@ -55,3 +55,52 @@
         }
 }
 
+
+ module.exports.sendDynamicTemplateMailBySendGrid = async function  ({ email, dynamic_template_data,template_id,id,token})  {
+
+
+        let data = JSON.stringify({
+            "personalizations": [
+                {
+                    "to": [
+                        {
+                            "email": email
+                        }
+                    ],
+                    "dynamic_template_data":dynamic_template_data
+                }
+            ],
+            "from": {
+                "email": "noreply@stellar-trust.com",
+                "name": "Stellar"
+            },
+            "template_id": template_id,
+            "reply_to": {
+                "email": "noreply@stellar-trust.com",
+                "name": "Stellar"
+            },
+            "headers": {
+                "ID": id
+            }
+        });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://api.sendgrid.com/v3/mail/send',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            data: data
+        };
+
+        const sendMailResponse = await axios.request(config);
+        if (sendMailResponse.status === 202||sendMailResponse.status === 200||sendMailResponse.status === 201){
+            return true;
+        } else {
+            console.error('Error sending email:', sendMailResponse.data);
+            return false;
+        }
+}
+
