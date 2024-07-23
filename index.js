7ee6c4550fbe9892307a8603b53ee9590a8a91ae
Abdullah <a.mohamed.anecdote@gmail.com>
2024-07-23 18:17:52 +0300

    implemented import bookings from lodgify

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const {updateBrevoContact} = require('./updateBrevoContact');
 const {getSmoobuNewAccessToken,getSmoobuApartments, getSmoobuBookingsByApartmentId} = require('./smoobu_utils');
 const {sendMailBySendGrid,sendDynamicTemplateMailBySendGrid} = require('./sendgrid_utils');
 const {sendEmail,getFormUrl,checkAndSendFormLinks} = require('./app_utils');
-const {importProperties} = require('./lodgify_utils');
+const {importProperties,getBookings} = require('./lodgify_utils');
 
-module.exports = {importProperties, updateBrevoContact, getSmoobuNewAccessToken,getSmoobuApartments, getSmoobuBookingsByApartmentId,sendMailBySendGrid, sendEmail,sendDynamicTemplateMailBySendGrid,getFormUrl,checkAndSendFormLinks };
+module.exports = {importProperties,getBookings, updateBrevoContact, getSmoobuNewAccessToken,getSmoobuApartments, getSmoobuBookingsByApartmentId,sendMailBySendGrid, sendEmail,sendDynamicTemplateMailBySendGrid,getFormUrl,checkAndSendFormLinks };
--- lodgify_utils.js
+++ lodgify_utils.js
@@ -17,11 +17,35 @@ module.exports.importProperties = async function ({apiKey}) {
     try {
         const response = await axios
             .request(options);
-        return response.data.items;
+        if (response.status === 200) {
+            return response.data.items;
+        } else {
+            throw response.data.message;
+        }
     } catch (error) {
         console.error('Failed to refresh token:', error.response ? error.response.data : error.message);
     }
 }
 
 
+module.exports.getBookings = async function ({apiKey }) {
 
+
+    const options = {
+        method: 'GET',
+        url: 'https://api.lodgify.com/v2/reservations/bookings?page=1&size=9999&includeCount=false&includeTransactions=false&includeExternal=false&includeQuoteDetails=false',
+        headers: {
+            accept: 'application/json',
+            'X-ApiKey': apiKey,
+        }
+    };
+
+
+    const response = await axios.request(options);
+    if (response.status === 200) {
+        return response.data.items;
+    } else {
+        throw response.data.message;
+    }
+
+}
