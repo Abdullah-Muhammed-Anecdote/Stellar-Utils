543b2e9cbcc8ecfb560a04beeb4a28fc9d3f362d
Abdullah <a.mohamed.anecdote@gmail.com>
2024-07-24 14:47:51 +0300

    updated booking api to fetch all bookings with pagination if exist

--- lodgify_utils.js
+++ lodgify_utils.js
@@ -24,18 +24,35 @@ module.exports.importProperties = async function ({apiKey}) {
 module.exports.getBookings = async function ({apiKey}) {
 
 
-    const options = {
-        method: 'GET',
-        url: 'https://api.lodgify.com/v2/reservations/bookings?page=1&size=999&includeCount=false&includeTransactions=false&includeExternal=false&includeQuoteDetails=false',
-        headers: {
-            accept: 'application/json',
-            'X-ApiKey': apiKey,
+    let page = 1;
+    let hasMore = true;
+    const allBookings = [];
+
+    while (hasMore) {
+        const options = {
+            method: 'GET',
+            url: `https://api.lodgify.com/v2/reservations/bookings?page=${page}&size=2147483647&includeCount=false&includeTransactions=false&includeExternal=false&includeQuoteDetails=false`, // Adjust 'size' as needed
+            headers: {
+                accept: 'application/json',
+                'X-ApiKey': apiKey,
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            if (response.data && response.data.items.length > 0) {
+                allBookings.push(...response.data.items);
+                page++; // Go to the next page if current page has data
+            } else {
+                hasMore = false; // Stop the loop if no data is returned
+            }
+        } catch (error) {
+            console.error('Failed to fetch bookings on page', page, ':', error);
+            break; // Exit the loop on error
         }
-    };
+    }
 
-
-    const response = await axios.request(options);
-    return response.data;
+    return allBookings;
 
 
 }
