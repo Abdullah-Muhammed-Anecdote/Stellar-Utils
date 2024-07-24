5bd5452c3c579a7f84caa29d774776547494580c
Abdullah <a.mohamed.anecdote@gmail.com>
2024-07-24 12:27:30 +0300

    solved maximum size in request

--- lodgify_utils.js
+++ lodgify_utils.js
@@ -3,19 +3,19 @@ const axios = require("axios");
 
 module.exports.importProperties = async function ({apiKey}) {
 
+    let options = {
 
-    const options = {
-        method: 'GET',
-        url: 'https://api.lodgify.com/v2/properties?includeCount=false&includeInOut=false&page=1&size=99999',
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://api.lodgify.com/v2/properties?includeCount=false&includeInOut=false&page=1&size=999',
         headers: {
-            accept: 'application/json',
+            'accept': 'application/json',
             'X-ApiKey': apiKey,
         }
     };
 
 
-    const response = await axios
-        .request(options);
+    const response = await axios.request(options);
     return response.data;
 
 }
@@ -26,7 +26,7 @@ module.exports.getBookings = async function ({apiKey}) {
 
     const options = {
         method: 'GET',
-        url: 'https://api.lodgify.com/v2/reservations/bookings?page=1&size=9999&includeCount=false&includeTransactions=false&includeExternal=false&includeQuoteDetails=false',
+        url: 'https://api.lodgify.com/v2/reservations/bookings?page=1&size=999&includeCount=false&includeTransactions=false&includeExternal=false&includeQuoteDetails=false',
         headers: {
             accept: 'application/json',
             'X-ApiKey': apiKey,
