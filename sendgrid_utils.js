a5f58bc2931dc72f094318d61270bedcbc9396b5
Abdullah <a.mohamed.anecdote@gmail.com>
2024-08-12 15:10:57 +0300

    added id to the reply to email instead of headers

--- sendgrid_utils.js
+++ sendgrid_utils.js
@@ -1,106 +1,112 @@
-
- const  axios  = require ("axios");
+const axios = require("axios");
 
 const replyToEmail = 'message@incoming.stellar-trust.com';
- module.exports.sendMailBySendGrid = async function  ({ email,subject, content,id,token})  {
+
+function getReplyToEmailID({id}) {
+    const splitReplyToEmail = replyToEmail.split('@');
+    return `${splitReplyToEmail[0]}+${id}@${splitReplyToEmail[1]}`;
+
+}
+
+module.exports.sendMailBySendGrid = async function ({email, subject, content, id, token}) {
 
 
-        let data = JSON.stringify({
-            "personalizations": [
-                {
-                    "to": [
-                        {
-                            "email": email
-                        }
-                    ],
-                    "subject": subject
-                }
-            ],
-            "content": [
-                {
-                    "type": "text/plain",
-                    "value": content
-                }
-            ],
-            "from": {
-                "email": replyToEmail,
-                "name": "Stellar"
-            },
-            "reply_to": {
-                "email": replyToEmail,
-                "name": "Stellar"
-            },
-            "headers": {
-                "ID": id
+    let data = JSON.stringify({
+        "personalizations": [
+            {
+                "to": [
+                    {
+                        "email": email
+                    }
+                ],
+                "subject": subject
             }
-        });
+        ],
+        "content": [
+            {
+                "type": "text/plain",
+                "value": content
+            }
+        ],
+        "from": {
+            "email": replyToEmail,
+            "name": "Stellar"
+        },
+        "reply_to": {
+            "email": getReplyToEmailID({id: id}),
+            "name": "Stellar"
+        },
+    });
 
-        let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: 'https://api.sendgrid.com/v3/mail/send',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            data: data
-        };
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://api.sendgrid.com/v3/mail/send',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        data: data
+    };
 
-        const sendMailResponse = await axios.request(config);
-        if (sendMailResponse.status === 202||sendMailResponse.status === 200||sendMailResponse.status === 201){
-            return true;
-        } else {
-            console.error('Error sending email:', sendMailResponse.data);
-            return false;
-        }
+    const sendMailResponse = await axios.request(config);
+    if (sendMailResponse.status === 202 || sendMailResponse.status === 200 || sendMailResponse.status === 201) {
+        return true;
+    } else {
+        console.error('Error sending email:', sendMailResponse.data);
+        return false;
+    }
 }
 
 
- module.exports.sendDynamicTemplateMailBySendGrid = async function  ({ email, dynamic_template_data,template_id,id,token})  {
+module.exports.sendDynamicTemplateMailBySendGrid = async function ({
+                                                                       email,
+                                                                       dynamic_template_data,
+                                                                       template_id,
+                                                                       id,
+                                                                       token
+                                                                   }) {
 
 
-        let data = JSON.stringify({
-            "personalizations": [
-                {
-                    "to": [
-                        {
-                            "email": email
-                        }
-                    ],
-                    "dynamic_template_data":dynamic_template_data
-                }
-            ],
-            "from": {
-                "email": replyToEmail,
-                "name": "Stellar"
-            },
-            "template_id": template_id,
-            "reply_to": {
-                "email": replyToEmail,
-                "name": "Stellar"
-            },
-            "headers": {
-                "ID": id
+    let data = JSON.stringify({
+        "personalizations": [
+            {
+                "to": [
+                    {
+                        "email": email
+                    }
+                ],
+                "dynamic_template_data": dynamic_template_data
             }
-        });
+        ],
+        "from": {
+            "email": replyToEmail,
+            "name": "Stellar"
+        },
+        "template_id": template_id,
+        "reply_to": {
+            "email": getReplyToEmailID({id: id}),
+            "name": "Stellar"
+        },
+    });
 
-        let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: 'https://api.sendgrid.com/v3/mail/send',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            data: data
-        };
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://api.sendgrid.com/v3/mail/send',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        data: data
+    };
 
-        const sendMailResponse = await axios.request(config);
-        if (sendMailResponse.status === 202||sendMailResponse.status === 200||sendMailResponse.status === 201){
-            return true;
-        } else {
-            console.error('Error sending email:', sendMailResponse.data);
-            return false;
-        }
+    const sendMailResponse = await axios.request(config);
+    if (sendMailResponse.status === 202 || sendMailResponse.status === 200 || sendMailResponse.status === 201) {
+        return true;
+    } else {
+        console.error('Error sending email:', sendMailResponse.data);
+        return false;
+    }
 }
 
